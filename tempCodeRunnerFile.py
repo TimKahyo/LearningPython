ed8b1b1b9a7097a8418a3d477513bc491e0e0be5
_Kaht <87621187+KahtKahyo@users.noreply.github.com>
2023-06-10 23:45:49 +0700

    new files

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+import sys
+import math
+import random
+import threading
+import time
+from functools import reduce
+
+name = input("What's your name")
+print("Hi", name, "!")
---
+++ variables.py
@@ -0,0 +1,13 @@
+firstVariable = 1 + 2 + 3
+secondVariable = 1 + 2 / 3
+
+print("Variable one: " + firstVariable)
+print("Variable two: " + secondVariable)
+
+# comments!
+# multiple statements
+
+first = 1
+second = 2
+third = 3
+print(first + second + third)
