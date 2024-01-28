1f20b5fa04962a70075c9c9afa0be073315c4c59
TimKahyo <87621187+KahtKahyo@users.noreply.github.com>
2024-01-28 21:37:35 +0700

    some comments

--- LearningModule/main.py
+++ LearningModule/main.py
@@ -1,5 +1,8 @@
 import secondary
 from secondary import squareAreaOfARectangle
+# or you can do this
+# from secondary import squareAreaOfARectangle, globalVariable
+
 
 firstRectangle = squareAreaOfARectangle(5, 10)
 
